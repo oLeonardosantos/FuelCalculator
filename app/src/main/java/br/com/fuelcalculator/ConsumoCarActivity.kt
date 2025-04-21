6d4aaf1619f24ba6706ca9488f7fdd87d5d4f95e
leonardo <leonardodossantosborges05@gmail.com>
2025-04-21 12:51:14 -0300

    adicionado novas imagens

--- app/src/main/java/br/com/fuelcalculator/ConsumoCarActivity.kt
+++ app/src/main/java/br/com/fuelcalculator/ConsumoCarActivity.kt
@@ -4,11 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputEditText
 
 class ConsumoCarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/br/com/fuelcalculator/PriceActivity.kt
+++ app/src/main/java/br/com/fuelcalculator/PriceActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 
 class PriceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +14,10 @@ class PriceActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.edt_preco)
         val btnProximo = findViewById<Button>(R.id.btn_proximo)
 
-            btnProximo.setOnClickListener {
-                val intent = Intent(this, ConsumoCarActivity::class.java)
-                intent.putExtra("preco", editText.text.toString().toFloat())
-                startActivity(intent)
-            }
+        btnProximo.setOnClickListener {
+            val intent = Intent(this, ConsumoCarActivity::class.java)
+            intent.putExtra("preco", editText.text.toString().toFloat())
+            startActivity(intent)
         }
     }
+}
